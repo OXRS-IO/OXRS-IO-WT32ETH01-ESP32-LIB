ecd0f186c8f4f5a9639ccaac4c921a470e5dbd0c
Ben Jones <sumnerboy12@users.noreply.github.com>
2023-07-17 22:13:37 +1200

    remove all home assistant self-discovery config changes (#3)

--- src/OXRS_WT32ETH01.h
+++ src/OXRS_WT32ETH01.h
@@ -42,11 +42,6 @@ class OXRS_WT32ETH01 : public Print
     boolean publishStatus(JsonVariant json);
     boolean publishTelemetry(JsonVariant json);
 
-    // Helpers for Home Assistant discovery
-    bool isHassDiscoveryEnabled();
-    void getHassDiscoveryJson(JsonVariant json, char * id);
-    bool publishHassDiscovery(JsonVariant json, char * component, char * id);
-
     // Implement Print.h wrapper
     virtual size_t write(uint8_t);
     using Print::write;
